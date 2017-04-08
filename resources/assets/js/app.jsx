813370307bc43010b317465fbdc994ac4bf5d61b
ImagneRabbits <mixxdarkz2@gmail.com>
2017-04-08 22:12:16 +0700

    add file

---
+++ resources/assets/js/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component{
+  constructor(){
+    super();
+  }
+
+  render(){
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <h1>test</h1>
+          <button className="btn btn-primary">test</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;   
---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>ReactDay - ToDoList</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <div id="root"></div>
+    
+    <script src="{{asset('js/vendor.js')}}"></script>
+    <script src="{{asset('js/bundle.js')}}"></script>
+  </body>
+</html>
