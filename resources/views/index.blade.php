17fa591adfd7eca545dfa9ae236b647442536c3a
ImagneRabbits <mixxdarkz2@gmail.com>
2017-04-08 23:46:46 +0700

    add file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
     <title>ReactDay - ToDoList</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
   </head>
   <body>
     <div id="root"></div>
