6705b088e48754475c7a43b99c69f97f21bf09d3
ImagneRabbits <mixxdarkz2@gmail.com>
2017-04-09 01:17:41 +0700

    add bootstrap manual

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -17776,12 +17776,34 @@ var WhatIs = function (_React$Component) {
     key: "render",
     value: function render() {
       return _react2.default.createElement(
-        "div",
-        { className: "container-fluid" },
+        "section",
+        { id: "whatis", className: "container-fluid" },
         _react2.default.createElement(
           "div",
           { className: "row" },
-          "Whatis"
+          _react2.default.createElement(
+            "div",
+            { className: "col-xs-12" },
+            _react2.default.createElement(
+              "h1",
+              null,
+              "WHAT IS BROWN BAG?"
+            ),
+            _react2.default.createElement(
+              "p",
+              null,
+              _react2.default.createElement(
+                "span",
+                null,
+                "Join us for two days of interactive demos, announcements, and best practices that will keep you looking ahead. Attend sessions and interact with Facebook products and experts. Whether you\u2019re with us in San Jose or tuning in from around the world, there\u2019s an F8 for everyone."
+              ),
+              _react2.default.createElement(
+                "span",
+                null,
+                "\u0E1A\u0E23\u0E32\u0E27\u0E41\u0E1A\u0E47\u0E04\u0E40\u0E1B\u0E47\u0E19\u0E07\u0E32\u0E19\u0E0A\u0E38\u0E21\u0E19\u0E38\u0E21\u0E23\u0E27\u0E21\u0E01\u0E25\u0E38\u0E48\u0E21\u0E2A\u0E33\u0E2B\u0E23\u0E31\u0E1A\u0E1C\u0E39\u0E49\u0E17\u0E35\u0E48\u0E2A\u0E19\u0E43\u0E08\u0E41\u0E1A\u0E48\u0E07\u0E1B\u0E31\u0E19\u0E04\u0E27\u0E32\u0E21\u0E23\u0E39\u0E49 \u0E1B\u0E23\u0E30\u0E2A\u0E1A\u0E01\u0E32\u0E23\u0E13\u0E4C \u0E2B\u0E23\u0E37\u0E2D\u0E40\u0E23\u0E37\u0E48\u0E2D\u0E07\u0E23\u0E32\u0E27\u0E15\u0E48\u0E32\u0E07\u0E46\u0E43\u0E2B\u0E49\u0E1C\u0E39\u0E49\u0E23\u0E48\u0E27\u0E21\u0E07\u0E32\u0E19\u0E04\u0E19\u0E2D\u0E37\u0E48\u0E19\u0E46 \u0E44\u0E14\u0E49\u0E1F\u0E31\u0E07\u0E01\u0E31\u0E19 \u0E07\u0E32\u0E19\u0E19\u0E35\u0E49\u0E44\u0E21\u0E48\u0E21\u0E35\u0E01\u0E32\u0E23\u0E01\u0E33\u0E2B\u0E19\u0E14\u0E2B\u0E31\u0E27\u0E02\u0E49\u0E2D\u0E40\u0E2D\u0E32\u0E44\u0E27\u0E49\u0E25\u0E48\u0E27\u0E07\u0E2B\u0E19\u0E49\u0E32 \u0E1C\u0E39\u0E49\u0E23\u0E48\u0E27\u0E21\u0E07\u0E32\u0E19\u0E17\u0E38\u0E01\u0E04\u0E19\u0E08\u0E30\u0E40\u0E1B\u0E47\u0E19\u0E1C\u0E39\u0E49\u0E01\u0E33\u0E2B\u0E19\u0E14\u0E2B\u0E31\u0E27\u0E02\u0E49\u0E2D\u0E43\u0E19\u0E07\u0E32\u0E19"
+              )
+            )
+          )
         )
       );
     }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('js/bootstrap.min.js')}}">
   </head>
   <body>
     <div id="root"></div>
