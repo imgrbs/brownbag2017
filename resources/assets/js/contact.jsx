5b47618fcd5cb5dba87e01a709ba71d7a17d5c64
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2017-04-09 04:46:06 +0700

    full width with fluid

--- resources/assets/js/contact.jsx
+++ resources/assets/js/contact.jsx
@@ -9,33 +9,36 @@ class Contact extends React.Component {
 
   render() {
     return (
-      <section id="contact" className="container">
-        <div className="row">
-          <div className="col-xs-12">
-            <img src={Bag} className="baglogo" alt=""/>
-          </div>
+      <section id="contact" className="container-fluid">
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12">
+              <img src={Bag} className="baglogo" alt=""/>
+            </div>
 
-          <div className="col-xs-12">
-            <h2>Brown Bag 2.0</h2>
-            <h3>by Alchemist</h3>
-          </div>
+            <div className="col-xs-12">
+              <h2>Brown Bag 2.0</h2>
+              <h3>by Alchemist</h3>
+            </div>
 
-          <div className="col-xs-12">
-            <h3>CONNECT WITH US</h3>
-          </div>
+            <div className="col-xs-12">
+              <h3>CONNECT WITH US</h3>
+            </div>
 
-          <div className="col-xs-12">
-            <i className="fa fa-facebook icon" style={{
-              paddingLeft:'0.7em',
-              paddingRight:'0.7em',
-            }}></i>
-            <i className="fa fa-twitter icon"></i>
-          </div>
-          <div className="col-xs-12">
-            <h4>CONTACT US</h4>
-            <h4>Alchemist@st.sit.kmutt.ac.th</h4>
+            <div className="col-xs-12">
+              <i className="fa fa-facebook icon" style={{
+                paddingLeft:'0.7em',
+                paddingRight:'0.7em',
+              }}></i>
+              <i className="fa fa-twitter icon"></i>
+            </div>
+            <div className="col-xs-12">
+              <h4>CONTACT US</h4>
+              <h4>Alchemist@st.sit.kmutt.ac.th</h4>
+            </div>
           </div>
         </div>
+
       </section>
     );
   }
--- resources/assets/js/nav.jsx
+++ resources/assets/js/nav.jsx
@@ -7,28 +7,29 @@ import React from 'react';
 
    render() {
      return (
-       <div>
-         <nav className="navbar navbar-default navbar-default-edit navbar-fixed-top">
-           <div className="container-fluid">
-             <div className="navbar-header">
-               <button type="button" className="navbar-toggle navbar-toggle-edit collapsed" data-toggle="collapse" data-target="#bbnav" aria-expanded="false">
-                 <span className="sr-only">Toggle navigation</span>
-                 <span className="icon-bar"></span>
-                 <span className="icon-bar"></span>
-                 <span className="icon-bar"></span>
-               </button>
-             </div>
+       <div className="container-fluid">
+         <div className="row">
+           <nav className="navbar navbar-default navbar-default-edit navbar-fixed-top">
+               <div className="navbar-header">
+                 <button type="button" className="navbar-toggle navbar-toggle-edit collapsed" data-toggle="collapse" data-target="#bbnav" aria-expanded="false">
+                   <span className="sr-only">Toggle navigation</span>
+                   <span className="icon-bar"></span>
+                   <span className="icon-bar"></span>
+                   <span className="icon-bar"></span>
+                 </button>
+               </div>
 
-            <div className="navbar-collapse navbar-collapse-edit collapse" id="bbnav" aria-expanded="false" style={{height: '1px'}}>
-              <ul className="nav navbar-nav">
-                <li className="active"><a href="#">WHAT'S</a></li>
-                <li><a href="#">TIMETABLE</a></li>
-                <li><a href="#">SESSIONS</a></li>
-              </ul>
-            </div>
+              <div className="navbar-collapse navbar-collapse-edit collapse" id="bbnav" aria-expanded="false" style={{height: '1px'}}>
+                <ul className="nav navbar-nav">
+                  <li className="active"><a href="#">WHAT'S</a></li>
+                  <li><a href="#">TIMETABLE</a></li>
+                  <li><a href="#">SESSIONS</a></li>
+                </ul>
+              </div>
 
-           </div>
-         </nav>
+           </nav>
+         </div>
+
        </div>
      );
    }
