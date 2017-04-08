61c84d5052143cbe0b300f523a9c00193a8ff8a2
ImagneRabbits <mixxdarkz2@gmail.com>
2017-04-09 01:23:28 +0700

    add git ignore

--- resources/assets/js/whatis.jsx
+++ resources/assets/js/whatis.jsx
@@ -11,10 +11,12 @@ class WhatIs extends React.Component{
         <div className="row">
             <div className="col-xs-12">
                 <h1>WHAT IS BROWN BAG?</h1>
-                <p>
+                <p>     
+                    <br/>
                     <span>
                         Join us for two days of interactive demos, announcements, and best practices that will keep you looking ahead. Attend sessions and interact with Facebook products and experts. Whether you’re with us in San Jose or tuning in from around the world, there’s an F8 for everyone.
                     </span>
+                    <br/><br/>
                     <span>
                         บราวแบ็คเป็นงานชุมนุมรวมกลุ่มสำหรับผู้ที่สนใจแบ่งปันความรู้ ประสบการณ์ หรือเรื่องราวต่างๆให้ผู้ร่วมงานคนอื่นๆ ได้ฟังกัน งานนี้ไม่มีการกำหนดหัวข้อเอาไว้ล่วงหน้า ผู้ร่วมงานทุกคนจะเป็นผู้กำหนดหัวข้อในงาน
                     </span>
