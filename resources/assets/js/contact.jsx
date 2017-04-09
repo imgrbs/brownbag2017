8b7485a7d4094726cecfa1b6a21979ff50565868
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2017-04-09 21:48:43 +0700

    solve overflow + new media rule

--- resources/assets/js/contact.jsx
+++ resources/assets/js/contact.jsx
@@ -10,29 +10,32 @@ class Contact extends React.Component {
   render() {
     return (
       <section id="contact" className="container-fluid">
-        <div className="container">
-          <div className="row">
-            <div className="col-xs-12 col-sm-4">
-              <img src={Bag} className="baglogo" alt=""/>
-              <h2>Brown Bag 2.0</h2>
-              <h3>by Alchemist</h3>
-            </div>
+        <div className="row">
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-12 col-sm-4">
+                <img src={Bag} className="baglogo" alt=""/>
+                <h2>Brown Bag 2.0</h2>
+                <h3>by Alchemist</h3>
+              </div>
 
-            <div className="col-xs-12 col-sm-4">
-              <h3>CONNECT WITH US</h3>
-              <i className="fa fa-facebook icon" style={{
-                paddingLeft:'0.7em',
-                paddingRight:'0.7em',
-              }}></i>
-              <i className="fa fa-twitter icon"></i>
-            </div>
+              <div className="col-xs-12 col-sm-4">
+                <h3>CONNECT WITH US</h3>
+                <i className="fa fa-facebook icon" style={{
+                  paddingLeft:'0.7em',
+                  paddingRight:'0.7em',
+                }}></i>
+                <i className="fa fa-twitter icon"></i>
+              </div>
 
-            <div className="col-xs-12 col-sm-4">
-              <h4>CONTACT US</h4>
-              <h4>Alchemist@st.sit.kmutt.ac.th</h4>
+              <div className="col-xs-12 col-sm-4">
+                <h3>CONTACT US</h3>
+                <h4>Alchemist@st.sit.kmutt.ac.th</h4>
+              </div>
             </div>
           </div>
         </div>
+
         <div className="endline"></div>
       </section>
     );
