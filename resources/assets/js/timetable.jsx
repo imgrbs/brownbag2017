849a8aafc3646a941ae5744906a843fbc6eadb90
ImagneRabbits <mixxdarkz2@gmail.com>
2017-04-09 22:06:57 +0700

    timetable desktop

--- resources/assets/js/timetable.jsx
+++ resources/assets/js/timetable.jsx
@@ -9,43 +9,45 @@ class TimeTable extends React.Component{
     return (
       <section id="timetable" className="container-fluid">
         <div className="row">
-          <div className="col-xs-12 col-md-10 col-md-offset-2">
+          <div className="col-xs-11 col-xs-offset-1 col-md-10 col-md-offset-2 timetable">
             <h1>TIMETABLE</h1>
             <h4>22 April 2017, KMUTT</h4>
           </div>
         </div>
-        <div className="row">
-          <Time time="13:00 - 13:30" />
-          <Event event="ลงทะเบียน" />
-        </div>
-        <div className="row">
-          <Time time="13:30 - 13:40" />
-          <Event event="พิธีเปิดโครงการ" />
-        </div>
-        <div className="row">
-          <Time time="13:40 - 14:20" />
-          <Event event="Session I" />
-        </div>
-        <div className="row">
-          <Time time="14:20 - 15:00" />
-          <Event event="Session II" />
-        </div>
-        <div className="row">
-          <Time time="15:00 - 15:30" />
-          <Event event="พักรับประทานอาหารว่าง และ" />
-          <Event event="แลกเปลี่ยนกันตามอัธยาศัย" />
-        </div>
-        <div className="row">
-          <Time time="15:30 - 16:10" />
-          <Event event="Session III" />
-        </div>
-        <div className="row">
-          <Time time="16:10 - 16:50" />
-          <Event event="Session IV" />
-        </div>
-        <div className="row">
-          <Time time="16:50 - 17:00" />
-          <Event event="สรุปงานและแลกเปลี่ยนกันตามอัธยาศัย" />
+        <div className="bodyTimetable">
+          <div className="row">
+            <Time time="13:00 - 13:30" />
+            <Event event="ลงทะเบียน" />
+          </div>
+          <div className="row">
+            <Time time="13:30 - 13:40" />
+            <Event event="พิธีเปิดโครงการ" />
+          </div>
+          <div className="row">
+            <Time time="13:40 - 14:20" />
+            <Event event="Session I" />
+          </div>
+          <div className="row">
+            <Time time="14:20 - 15:00" />
+            <Event event="Session II" />
+          </div>
+          <div className="row">
+            <Time time="15:00 - 15:30" />
+            <Event event="พักรับประทานอาหารว่าง และ" />
+            <Event event="แลกเปลี่ยนกันตามอัธยาศัย" />
+          </div>
+          <div className="row">
+            <Time time="15:30 - 16:10" />
+            <Event event="Session III" />
+          </div>
+          <div className="row">
+            <Time time="16:10 - 16:50" />
+            <Event event="Session IV" />
+          </div>
+          <div className="row">
+            <Time time="16:50 - 17:00" />
+            <Event event="สรุปงานและแลกเปลี่ยนกันตามอัธยาศัย" />
+          </div>
         </div>
       </section>
     );
@@ -55,7 +57,7 @@ class TimeTable extends React.Component{
 class Time extends React.Component{
   render(){
     return (
-      <div className="col-xs-12 col-md-2 col-md-offset-2 time">{this.props.time}</div>
+      <div className="col-xs-11 col-xs-offset-1 col-md-2 col-md-offset-2 time">{this.props.time}</div>
     );
   }
 }
@@ -63,7 +65,7 @@ class Time extends React.Component{
 class Event extends React.Component{
   render(){
     return (
-      <div className="col-xs-12 col-md-8 event">{this.props.event}</div>
+      <div className="col-xs-11 col-xs-offset-1 col-md-6 event">{this.props.event}</div>
     );
   }
 }
