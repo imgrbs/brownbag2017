240b19b01ae64f8580b293d48cba2b7337485825
ImagneRabbits <mixxdarkz2@gmail.com>
2017-04-09 19:13:22 +0700

    edit first sec

--- resources/assets/js/main.jsx
+++ resources/assets/js/main.jsx
@@ -17,15 +17,25 @@ class WhatIs extends React.Component{
             </div>
             <div className="col-xs-12 boxDetail text-center">
                 <h1>Brown Bag 2.0</h1>
-                <p>
-                    <h2>22 April 2017</h2>
-                    <h4>13:00 PM - 17:00 PM</h4>
-                    <h2>SIT Building</h2>
-                    <h4>KMUTT</h4>
+                <p className="">
+                    <span className="col-xs-2 col-xs-offset-2 icon-main">
+                        <i className="fa fa-clock-o" aria-hidden="true"></i>
+                    </span>
+                    <span className="col-xs-7 detail-main">
+                        <h2>22 April 2017</h2>
+                        <h4>13:00 PM - 17:00 PM</h4>
+                    </span>
+                    <span className="col-xs-2 col-xs-offset-2 icon-main">
+                        <i className="fa fa-map-marker" aria-hidden="true"></i>
+                    </span>
+                    <span className="col-xs-7 detail-main">
+                        <h2>SIT Building</h2>
+                        <h4>KMUTT</h4>
+                    </span>
                 </p>
                 <div className="col-xs-12 text-center mainMenu">
-                    <a href="" className="">WHAT'S</a>
-                    <a href="" className="">SESSIONS</a>
+                    <a href="#" className="whatis-btn">WHAT'S</a>
+                    <a href="#" className="">SESSIONS</a>
                 </div>
             </div>
         </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
     <title>Brown Bag 2.0</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('js/bootstrap.min.js')}}">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
