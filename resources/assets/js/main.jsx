3263441075b72f6ce12d43642feae1341ebacb55
ImagneRabbits <mixxdarkz2@gmail.com>
2017-04-09 20:17:08 +0700

    fix first sec css

--- resources/assets/js/main.jsx
+++ resources/assets/js/main.jsx
@@ -19,10 +19,7 @@ class WhatIs extends React.Component{
                 <h1>Brown Bag 2.0</h1>
                 <div className="row">
                   <div className="col-xs-offset-2 col-xs-2">
-                    <i className="fa fa-clock-o" aria-hidden="true" style={{
-                      marginTop:'0.1em',
-                      marginLeft:'-0.1em'
-                    }}></i>
+                    <i className="fa fa-clock-o" aria-hidden="true"></i>
                   </div>
                   <div className="col-xs-8">
                     <h2>22 April 2017</h2>
@@ -39,22 +36,7 @@ class WhatIs extends React.Component{
                     <h4>KMUTT</h4>
                   </div>
                 </div>
-                {/* <p className="">
-                    <span className="col-xs-2 col-xs-offset-2 icon-main">
-                    </span>
-                    <span className="col-xs-7 detail-main">
-                        <h2>22 April 2017</h2>
-                        <h4>13:00 PM - 17:00 PM</h4>
-                    </span>
-                    <span className="col-xs-2 col-xs-offset-2 icon-main">
-                        <i className="fa fa-map-marker" aria-hidden="true"></i>
-                    </span>
-                    <span className="col-xs-7 detail-main">
-                        <h2>SIT Building</h2>
-                        <h4>KMUTT</h4>
-                    </span>
-                </p> */}
-                <div className="row" style={{marginTop:'1.4em'}}>
+                <div className="row">
                   <div className="col-xs-12 text-center mainMenu">
                       <a href="#" className="whatis-btn">WHAT'S</a>
                       <a href="#" className="session-btn">SESSIONS</a>
