8b341a891413398ac37225efe67e88f065cf78b7
ImagneRabbits <mixxdarkz2@gmail.com>
2017-04-11 01:41:03 +0700

    edit link + edit # 2.0

--- resources/assets/js/contact.jsx
+++ resources/assets/js/contact.jsx
@@ -17,7 +17,7 @@ class Contact extends React.Component {
               <div className="col-xs-12 col-sm-3 col-sm-offset-1 col-md-5 col-md-offset-1">
                 <div className="col-md-3"><img src={Bag} className="baglogo" alt=""/></div>
                 <div className="col-md-9 text-left">
-                  <h2>Brown Bag 2.0</h2>
+                  <h2>Brown Bag #2.0</h2>
                   <h3>by Alchemist</h3>
                 </div>
               </div>
@@ -25,10 +25,10 @@ class Contact extends React.Component {
                 <h3>CONTACT US</h3>
                 <h4>alchemist@sit.kmutt.ac.th</h4>
                 <br/>
-                <a href="https://sitbrownbag.eventbrite.com">
+                <a href="https://fb.com/BrownBagSITKMUTT/">
                 <img src={Evb} className="evbLogo" alt=""/>
                 </a>
-                <a href="https://fb.com/BrownBagSITKMUTT/">
+                <a href="https://sitbrownbag.eventbrite.com">
                 <img src={Fb} className="evbLogo" alt=""/>
                 </a>
               </div>
